bbf0d428a1abb9e8429a590ba7898c01d2026e8d
Eddie Ramirez <87774596+EddieASC21@users.noreply.github.com>
2026-03-29 18:08:35 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)) return new int[]{map.get(diff), i};
+            else map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
+
+/*
+rather simple
+
+what we must do is check is use a hashmap
+
+as while we iterate through the array 
+
+we want to see if the element we on can be added with any element seen to add to the target
+
+we use a hashmap as we want to keep track of the index as thats what we must return 
+*/
